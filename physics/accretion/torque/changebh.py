b0ceca9b5db6374eb3166471dc61d61a606adc4a
bmckernan <bmckernan@amnh.org>
2023-02-03 15:02:51 -0500

    added physics.accretion.torque changebh tests of spin and spin angle change

--- physics/accretion/torque/changebh.py
+++ physics/accretion/torque/changebh.py
@@ -1,5 +1,5 @@
-def change_spin_magnitudes(prograde_bh_spins,frac_Eddington_ratio,spin_torque_condition,mass_growth_Edd_rate,timestep):
-#def change_spin_magnitudes(bh_spins,prograde_orb_ang_mom_indices,frac_Eddington_ratio,spin_torque_condition,mass_growth_Edd_rate,timestep):
+def change_spin_magnitudes(prograde_bh_spins,frac_Eddington_ratio,spin_torque_condition,timestep):
+    #def change_spin_magnitudes(bh_spins,prograde_orb_ang_mom_indices,frac_Eddington_ratio,spin_torque_condition,mass_growth_Edd_rate,timestep):
     #bh_new_spins=bh_spins
     normalized_Eddington_ratio=frac_Eddington_ratio/1.0
     normalized_timestep=timestep/1.e4
@@ -7,8 +7,8 @@ def change_spin_magnitudes(prograde_bh_spins,frac_Eddington_ratio,spin_torque_co
    
     #bh_new_spins[prograde_orb_ang_mom_indices]=bh_new_spins[prograde_orb_ang_mom_indices]+(4.4e-3*normalized_Eddington_ratio*normalized_spin_torque_condition*normalized_timestep)
     bh_new_spins=prograde_bh_spins+(4.4e-3*normalized_Eddington_ratio*normalized_spin_torque_condition*normalized_timestep)
-# TO DO: Include a condition to keep a maximal (a=+0.98) spin BH at that value once it reaches it
-#Return updated new spins    
+    # TO DO: Include a condition to keep a maximal (a=+0.98) spin BH at that value once it reaches it
+    #Return updated new spins    
     return bh_new_spins
 
 def change_spin_angles(prograde_bh_spin_angles,frac_Eddington_ratio,spin_torque_condition,spin_minimum_resolution,timestep):
