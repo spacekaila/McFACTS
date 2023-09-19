f731e2cd58d951eaae8a09879d5c65db9894ba03
richard.oshaughnessy <richard.oshaughnessy@ligo.org>
2023-09-19 16:10:46 -0400

    tichy08: updat eto vectorize

--- physics/binary/merge/tichy08.py
+++ physics/binary/merge/tichy08.py
@@ -1,29 +1,32 @@
 
+import numpy as np
 
-def merged_mass(mass_1,mass_2,spin_1,spin_2):
+def merged_mass(m1,m2,spin_1,spin_2):
 # Calculate the final mass of a merged binary
 # Only depends on M1,M2,a1,a2
 # Using approximations from Tichy \& Maronetti (2008) where
 # m_final=(M1+M2)*[1.0-0.2nu-0.208nu^2(a1+a2)]
 # where nu is the symmetric mass ratio or nu=q/((1+q)^2)
-    total_bin_mass = mass_1 + mass_2
-    total_spin = spin_1 + spin_2
-    
-    if mass_1 >= mass_2:
-        primary = mass_1
-        secondary = mass_2
+    # These are arrays
+    mass_1 = -1
+    mass_2 = -1
+    if isinstance(m1, np.ndarray):
+     if len(m1.shape) >0:
+        mass_1 = m1[0]
+        mass_2 = m2[0]
+        total_spin = spin_1[0] + spin_2[0]
     else:
-        primary = mass_2
-        secondary = mass_1
-    # Mass ratio should always be <1. Include a TEST making sure that's TRUE
+        mass_1 = m1
+        mass_2 = m2
+        total_spin = spin_1 + spin_2
     
-    mass_ratio = secondary/primary
-    nu_factor = (1.0+mass_ratio)**2.0
-    nu = mass_ratio/nu_factor
+
+    print(" Mass array ",mass_1 + mass_2)
+    nu =  (mass_1* mass_2)/ (mass_1+mass_2)**2 # mass_ratio/nu_factor
     nusq = nu*nu
 
     mass_factor = 1.0-(0.2*nu)-(0.208*nusq*total_spin)
-    merged_mass = total_bin_mass*mass_factor
+    merged_mass = (mass_1+mass_2)*mass_factor
     return merged_mass
 
 
@@ -33,12 +36,15 @@ def merged_spin(mass_1, mass_2, spin_1, spin_2, bin_ang_mom):
 # Using approximations from Tichy \& Maronetti(2008) where
 # a_final=0.686(5.04nu-4.16nu^2) +0.4[a1/((0.632+1/q)^2)+ a2/((0.632+q)^2)]
 # where q=m_2/m_1 and nu=q/((1+q)^2)
-    if mass_1 >= mass_2:
-        primary = mass_1
-        secondary = mass_2
-    else:
-        primary = mass_2
-        secondary = mass_1
+    # if mass_1 >= mass_2:
+    #     primary = mass_1
+    #     secondary = mass_2
+    # else:
+    #     primary = mass_2
+    #     secondary = mass_1
+    primary = np.max(np.c_[mass_1, mass_2])
+    secondary = np.min(np.c_[mass_1, mass_2])
+
     mass_ratio = secondary/primary
     inv_mass_ratio = (1.0/mass_ratio)
     nu_factor = (1.0+mass_ratio)**2.0
