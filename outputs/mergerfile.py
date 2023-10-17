141f44e8c7a5bb4baf474c70ad886a59b39629f0
bmckernan <bmckernan@amnh.org>
2023-10-17 12:21:23 -0400

    corrected merged_bh

--- outputs/mergerfile.py
+++ outputs/mergerfile.py
@@ -44,6 +44,7 @@ def merged_bh(merged_array,bin_array, merger_indices_here ,chi_here,mass_here,sp
     """
     Recording merger event.  Designed only to handle one event at a time, with hardcoded labels
     """
+    #Comment here
     #Return an array with properties of merger
     #Center of mass, M_total, Chi_eff, a_tot, spin_angle, m1,m2,a1,a2,theta1,theta2,gen1,gen2,time of merger
     merger_indices = np.array(merger_indices_here)
