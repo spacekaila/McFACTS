d7e2cd30290dc228773bebfa81bc9bf1e75fe149
Kaila Nathaniel <37944273+spacekaila@users.noreply.github.com>
2024-09-09 17:39:03 -0400

    linting

--- src/mcfacts/setup/initializediskstars.py
+++ src/mcfacts/setup/initializediskstars.py
@@ -38,13 +38,13 @@ def init_single_stars(opts,id_start_val = None):
     masses_initial_sorted = masses_initial[sort_idx]
 
     masses_stars, r_locations_stars = diskstars_hillspheremergers.hillsphere_mergers(n_stars=n_stars_initial,
-                                                   masses_initial_sorted=masses_initial_sorted,
-                                                   r_locations_initial_sorted=r_locations_initial_sorted,
-                                                   min_initial_star_mass=opts.disk_star_mass_min_init,
-                                                   R_disk = opts.disk_radius_trap,
-                                                   M_smbh=opts.smbh_mass,
-                                                   P_m = 1.35,
-                                                   P_r = 1.)
+                                                                                     masses_initial_sorted=masses_initial_sorted,
+                                                                                     r_locations_initial_sorted=r_locations_initial_sorted,
+                                                                                     min_initial_star_mass=opts.disk_star_mass_min_init,
+                                                                                     R_disk = opts.disk_radius_trap,
+                                                                                     M_smbh=opts.smbh_mass,
+                                                                                     P_m = 1.35,
+                                                                                     P_r = 1.)
     n_stars = len(masses_stars)
     
     star_radius = setupdiskstars.setup_disk_stars_radii(masses_stars)
@@ -60,20 +60,19 @@ def init_single_stars(opts,id_start_val = None):
     star_Y = opts.nsc_star_metallicity_y_init
     star_Z = opts.nsc_star_metallicity_z_init
 
-    stars = AGNStar(mass = masses_stars,
-                        spin = star_spin,
-                        spin_angle = star_spin_angle,
-                        orbit_a = r_locations_stars, #this is location
-                        orbit_inclination = star_orbit_inclination,
-                        orbit_e = star_orbit_e,
-                        #orb_ang_mom = star_orb_ang_mom,
-                        orbit_arg_periapse = star_orbit_argperiapse,
-                        star_radius = star_radius,
-                        star_Y = star_Y,
-                        star_Z = star_Z,
-                        mass_smbh = opts.smbh_mass,
-                        id_start_val=id_start_val,
-                        n_stars = n_stars)
-
-    
+    stars = AGNStar(mass=masses_stars,
+                    spin=star_spin,
+                    spin_angle=star_spin_angle,
+                    orbit_a=r_locations_stars, #this is location
+                    orbit_inclination=star_orbit_inclination,
+                    orbit_e=star_orbit_e,
+                    #orb_ang_mom= star_orb_ang_mom,
+                    orbit_arg_periapse=star_orbit_argperiapse,
+                    star_radius=star_radius,
+                    star_Y=star_Y,
+                    star_Z=star_Z,
+                    mass_smbh=opts.smbh_mass,
+                    id_start_val=id_start_val,
+                    n_stars=n_stars)
+   
     return(stars,n_stars)
