a929d51fa875723bab0e1195f9c506aa579b0182
bmckernan <bmckernan@amnh.org>
2024-09-10 10:45:14 -0400

    updated var names in changebhmass,changebh

--- src/mcfacts/physics/accretion/eddington/changebhmass.py
+++ src/mcfacts/physics/accretion/eddington/changebhmass.py
@@ -1,29 +1,29 @@
 import numpy as np
 
 
-def change_mass(prograde_bh_masses, frac_Eddington_ratio, mass_growth_Edd_rate, timestep):
+def change_mass(disk_bh_pro_masses, disk_bh_eddington_ratio, disk_bh_eddington_mass_growth_rate, timestep_duration_yr):
     """Given initial black hole masses at start of timestep, add mass according to
         chosen BH mass accretion prescription
 
     Parameters
     ----------
-    prograde_bh_masses : float array
+    disk_bh_pro_masses : float array
         initial masses of black holes in prograde orbits around SMBH, units of solar masses
-    frac_Eddington_ratio : float
+    disk_bh_eddington_ratio : float
         user chosen input set by input file; Accretion rate of fully embedded stellar mass 
         black hole in units of Eddington accretion rate. 1.0=embedded BH accreting at Eddington.
         Super-Eddington accretion rates are permitted.
-    mass_growth_Edd_rate : float
+    mdisk_bh_eddington_mass_growth_rate : float
         fractional rate of mass growth AT Eddington accretion rate per year (2.3e-8)
-    timestep : float
+    timestep_duration_yr : float
         length of timestep in units of years
 
     Returns
     -------
-    bh_new_masses : float array
+    disk_bh_pro_new_masses : float array
         masses of black holes after accreting at prescribed rate for one timestep
     """
     # Mass grows exponentially for length of timestep:
-    bh_new_masses = prograde_bh_masses*np.exp(mass_growth_Edd_rate*frac_Eddington_ratio*timestep)
+    disk_bh_pro_new_masses = disk_bh_pro_masses*np.exp(disk_bh_eddington_mass_growth_rate*disk_bh_eddington_ratio*timestep_duration_yr)
 
-    return bh_new_masses
+    return disk_bh_pro_new_masses
