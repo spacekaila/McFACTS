d02287c2786485b196881a5a00e812a010a8643d
Harrison Cook <hecook@nmsu.edu>
2024-09-10 10:23:38 -0400

    removed unused function bin_tgw()

--- src/mcfacts/physics/binary/merge/tgw.py
+++ src/mcfacts/physics/binary/merge/tgw.py
@@ -1,30 +1,6 @@
 import numpy as np
 
 
-def bin_tgw(mass_1,mass_2,location_1,location_2,norm_t_gw):
-    # Calculate the t_gw from Peters(1964), which is
-    # t_gw~ (5/128)*(c^5/G^3)*(a^4/M_b^2 mu_b)*(1-e^2)^(7/2)
-    # where M_b=M1+M2 (bin mass) and mu_b=M1M2/(M1+M2) the reduced binary mass
-    # and can be parameterized as
-    # t_gw=6.1e12yr(1-e^2)^(7/2)*(a/10^3r_g)^4 (M_smbh/10^8Msun)^3 (m_bh/10Msun)^-1
-    bin_mass = mass_1 + mass_2
-    bin_reduced_mass = (mass_1*mass_2)/bin_mass
-    bin_separation = np.abs(location_1-location_2)
-
-    scaled_location = 1.e3
-    scaled_bin_mass = 10.0
-    scaled_smbh_mass = 1.e8
-    #normalized_locations=bh_locations/scaled_location
-    #normalized_masses=bh_masses/scaled_mass
-    #normalized_smbh_mass = mass_smbh/scaled_smbh_mass
-    #eccentricity factor (1-e^2)^7/2 is
-    #ecc_factor=(1.0-(bh_orbital_eccentricities)**(2.0))**(3.5)
-    #time_for_gw_decay=6.1e12*ecc_factor*(normalized_locations**4.0)*(normalized_smbh_mass**2.0)*(1.0/normalized_masses)
-    #     bin_t_gw=norm_t_gw*((bin_separation)^(4.0))*((m_smbh/1.e8)^(4.0))*((bin_mass/10.0)^(-2.0))*((bin_reduced_mass/2.5)^(-1.0))
-    #return time_for_gw_decay
-    return
-
-
 def normalize_tgw(mass_smbh):
     #Calculate the normalization for timescale of merger (in yrs) due to GW emission.
     #From Peters(1964) t_gw approx (5/128)* c^5/G^3 *a_b^4/(M_{b}^{2}mu_{b})
