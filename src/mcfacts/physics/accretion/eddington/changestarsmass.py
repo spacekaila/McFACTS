399730b08725dfbe12cce6915ba406e15c609bfc
Kaila Nathaniel <37944273+spacekaila@users.noreply.github.com>
2024-09-13 10:54:03 -0400

    linted and changed variable names

--- src/mcfacts/physics/accretion/eddington/changestarsmass.py
+++ src/mcfacts/physics/accretion/eddington/changestarsmass.py
@@ -1,29 +1,32 @@
 import numpy as np
 
 
-def change_mass(prograde_stars_masses, frac_Eddington_ratio, mass_growth_Edd_rate, timestep):
-    """Given initial black hole masses at start of timestep, add mass according to
+def change_mass(disk_star_pro_masses,
+                disk_star_eddington_ratio,
+                mdisk_star_eddington_mass_growth_rate,
+                timestep_duration_yr):
+    """Given initial star masses at start of timestep, add mass according to
         chosen stellar mass accretion prescription
 
     Parameters
     ----------
-    prograde_stars_masses : float array
+    disk_star_pro_masses : float array
         initial masses of stars in prograde orbits around SMBH, units of solar masses
-    frac_Eddington_ratio : float
+    disk_star_eddington_ratio : float
         user chosen input set by input file; Accretion rate of fully embedded stars
           in units of Eddington accretion rate. 1.0=embedded star accreting at Eddington.
         Super-Eddington accretion rates are permitted.
-    mass_growth_Edd_rate : float
+    mdisk_star_eddington_mass_growth_rate : float
         fractional rate of mass growth AT Eddington accretion rate per year (2.3e-8)
-    timestep : float
+    timestep_duration_yr : float
         length of timestep in units of years
 
     Returns
     -------
-    bh_new_masses : float array
-        masses of black holes after accreting at prescribed rate for one timestep
+    disk_star_pro_new_masses : float array
+        masses of stars after accreting at prescribed rate for one timestep
     """
     # Mass grows exponentially for length of timestep:
-    stars_new_masses = prograde_stars_masses*np.exp(mass_growth_Edd_rate*frac_Eddington_ratio*timestep)
+    disk_star_pro_new_masses = disk_star_pro_masses*np.exp(mdisk_star_eddington_mass_growth_rate*disk_star_eddington_ratio*timestep_duration_yr)
 
-    return stars_new_masses
+    return disk_star_pro_new_masses
