9aafd52ec057cc5081af8fd83d8544fab41e5b81
Vera Delfavero <vera.delfavero.nasa.gov>
2024-09-24 17:00:00 -0400

    updated local things

--- scripts/plot_disk_properties.py
+++ scripts/plot_disk_properties.py
@@ -64,27 +64,28 @@ def ROS_plots():
     plt.plot(np.log(rvals), (Sigma1/Sigma2))
     plt.savefig("Sigma_ratio.png")
 
-def Vera_batch_rho(
+def pAGN_model_batch(
     disk_model_name="sirko_goodman",
-    disk_radius_outer=5000,
+    disk_radius_outer=50000,
     rad_efficiency=0.1,
     ):
     disk_alpha_viscositys = np.asarray([0.1, 0.5])
     edd_ratio = np.asarray([0.5,0.1])
     smbh_masses = 10**np.asarray([6,7,8,9])
-    # Setup plots
-    fig, _axes = plt.subplots(
-        figsize=(12,9),
-        nrows=disk_alpha_viscositys.size,ncols=edd_ratio.size
-        )
-    plt.style.use('bmh')
-    # Plot things
+    # Define dictionaries
+    surface_density_dict = {}
+    aspect_ratio_dict = {}
+    opacity_dict = {}
+    pAGN_model_dict = {}
+    bonus_structures_dict = {}
+
+    # Loop through pAGN models
     for _id_alpha, _alpha  in enumerate(disk_alpha_viscositys):
         for _id_edd,_edd in enumerate(edd_ratio):
-            # Identify axis
-            ax = _axes[_id_alpha, _id_edd]
             # Loop SMBH mass
             for _id_mass, _mass in enumerate(smbh_masses):
+                # Identify pAGN model
+                tag = "%s_%s_%s"%(str(_alpha),str(_edd),str(_mass))
                 print("alpha:", _alpha)
                 print("edd:", _edd)
                 print("mass:",_mass)
@@ -99,21 +100,53 @@ def Vera_batch_rho(
                             _edd,
                             rad_efficiency,
                         )
-                    # Identify label
-                    if (_id_alpha == 0) and (_id_edd == 0):
-                        label = "$M_{\mathrm{SMBH}} = 10^{%0.1f}$"%(np.log10(_mass))
-                    else:
-                        label = None
-                    # plot things
-                    ax.plot(np.log10(bonus_structures['R']),np.log10(bonus_structures['rho']),label=label)
+                    # Add things to dictionary
+                    surface_density_dict[tag] = surface_density_func
+                    aspect_ratio_dict[tag] = aspect_ratio_func
+                    opacity_dict[tag] = opacity_func
+                    pAGN_model_dict[tag] = pAGN_model
+                    bonus_structures_dict[tag] = bonus_structures
                 except:
                     line = "Failed building pAGN disk model for "
                     line = line + "disk_alpha_viscosity = " + str(_alpha) + "; "
                     line = line + "edd = " + str(_edd) + "; "
                     line = line + "mass = " + str(_mass)
                     print(line)
+            
+    ### Density plot ###
+    # Setup density plot
+    fig, _axes = plt.subplots(
+        figsize=(12,9),
+        nrows=disk_alpha_viscositys.size,ncols=edd_ratio.size
+        )
+    plt.style.use('bmh')
+    # Loop through pAGN models
+    for _id_alpha, _alpha  in enumerate(disk_alpha_viscositys):
+        for _id_edd,_edd in enumerate(edd_ratio):
+            # Identify axis
+            ax = _axes[_id_alpha, _id_edd]
+            # Loop SMBH mass
+            for _id_mass, _mass in enumerate(smbh_masses):
+                # Identify pAGN model
+                tag = "%s_%s_%s"%(str(_alpha),str(_edd),str(_mass))
+                # Identify label
+                if (_id_alpha == 0) and (_id_edd == 0):
+                    label = "$M_{\mathrm{SMBH}} = 10^{%0.1f}$"%(np.log10(_mass))
+                else:
+                    label = None
+                # Check if the pAGN model is really available
+                if not (tag in surface_density_dict):
+                    # If not, keep the legend consistent and move on
                     ax.plot([],[],label=label)
-                
+                    continue
+                # Otherwise, plot things
+                ax.plot(np.log10(bonus_structures_dict[tag]['R']),np.log10(bonus_structures_dict[tag]['rho']),label=label)
+
+            # Vertical line for disk_radius_outer
+            ylims = ax.get_ylim()
+            ax.vlines(np.log10(disk_radius_outer), ylims[0],ylims[1], color='black', label="Truncate", linestyle='dashed')
+            ax.set_ylim(ylims)
+
             # Generate a title
             title = r"$ \alpha = %0.3f; \mathrm{Edd} = %0.3f$"%(_alpha, _edd)
             ax.set_title(title)
@@ -124,63 +157,47 @@ def Vera_batch_rho(
             # axis labels
             ax.set_xlabel(r"$\log_{10}(R)$")
             ax.set_ylabel(r"$\log_{10}(\rho)$")
-            
     # show plots
     #plt.tight_layout()
     savename = "pAGN_%s_disk_rho.png"%(disk_model_name)
     fig.savefig(savename)
     #plt.show()
+    plt.close()
 
-def Vera_batch_T(
-    disk_model_name="sirko_goodman",
-    disk_radius_outer=5000,
-    rad_efficiency=0.1,
-    ):
-    disk_alpha_viscositys = np.asarray([0.1, 0.5])
-    edd_ratio = np.asarray([0.5,0.1])
-    smbh_masses = 10**np.asarray([6,7,8,9])
-    # Setup plots
+    ### Temperature plot ###
+    # Setup density plot
     fig, _axes = plt.subplots(
         figsize=(12,9),
         nrows=disk_alpha_viscositys.size,ncols=edd_ratio.size
         )
     plt.style.use('bmh')
-    # Plot things
+    # Loop through pAGN models
     for _id_alpha, _alpha  in enumerate(disk_alpha_viscositys):
         for _id_edd,_edd in enumerate(edd_ratio):
             # Identify axis
             ax = _axes[_id_alpha, _id_edd]
             # Loop SMBH mass
             for _id_mass, _mass in enumerate(smbh_masses):
-                print("disk_alpha_viscosity:", _alpha)
-                print("edd:", _edd)
-                print("mass:",_mass)
-                try:
-                    # Generate model
-                    surface_density_func, aspect_ratio_func, opacity_func, pAGN_model, bonus_structures = \
-                        construct_disk_pAGN(
-                            disk_model_name,
-                            _mass,
-                            disk_radius_outer,
-                            _alpha,
-                            _edd,
-                            rad_efficiency,
-                        )
-                    # Identify label
-                    if (_id_alpha == 0) and (_id_edd == 0):
-                        label = "$M_{\mathrm{SMBH}} = 10^{%0.1f}$"%(np.log10(_mass))
-                    else:
-                        label = None
-                    # plot things
-                    ax.plot(np.log10(bonus_structures['R']),np.log10(bonus_structures['T']),label=label)
-                except:
-                    line = "Failed building pAGN disk model for "
-                    line = line + "disk_alpha_viscosity = " + str(_alpha) + "; "
-                    line = line + "edd = " + str(_edd) + "; "
-                    line = line + "mass = " + str(_mass)
-                    print(line)
+                # Identify pAGN model
+                tag = "%s_%s_%s"%(str(_alpha),str(_edd),str(_mass))
+                # Identify label
+                if (_id_alpha == 0) and (_id_edd == 0):
+                    label = "$M_{\mathrm{SMBH}} = 10^{%0.1f}$"%(np.log10(_mass))
+                else:
+                    label = None
+                # Check if the pAGN model is really available
+                if not (tag in surface_density_dict):
+                    # If not, keep the legend consistent and move on
                     ax.plot([],[],label=label)
-                
+                    continue
+                # Otherwise, plot things
+                ax.plot(np.log10(bonus_structures_dict[tag]['R']),np.log10(bonus_structures_dict[tag]['T']),label=label)
+
+            # Vertical line for disk_radius_outer
+            ylims = ax.get_ylim()
+            ax.vlines(np.log10(disk_radius_outer), ylims[0],ylims[1], color='black', label="Truncate", linestyle='dashed')
+            ax.set_ylim(ylims)
+
             # Generate a title
             title = r"$ \alpha = %0.3f; \mathrm{Edd} = %0.3f$"%(_alpha, _edd)
             ax.set_title(title)
@@ -191,63 +208,59 @@ def Vera_batch_T(
             # axis labels
             ax.set_xlabel(r"$\log_{10}(R)$")
             ax.set_ylabel(r"$\log_{10}(T)$")
-            
     # show plots
     #plt.tight_layout()
     savename = "pAGN_%s_disk_T.png"%(disk_model_name)
     fig.savefig(savename)
     #plt.show()
+    plt.close()
 
-def Vera_batch_tauV(
-    disk_model_name="sirko_goodman",
-    disk_radius_outer=5000,
-    rad_efficiency=0.1,
-    ):
-    disk_alpha_viscositys = np.asarray([0.1, 0.5])
-    edd_ratio = np.asarray([0.5,0.1])
-    smbh_masses = 10**np.asarray([6,7,8,9])
-    # Setup plots
+    ### Opacity plot ###
+    # Setup density plot
     fig, _axes = plt.subplots(
         figsize=(12,9),
         nrows=disk_alpha_viscositys.size,ncols=edd_ratio.size
         )
     plt.style.use('bmh')
-    # Plot things
+    # Loop through pAGN models
     for _id_alpha, _alpha  in enumerate(disk_alpha_viscositys):
         for _id_edd,_edd in enumerate(edd_ratio):
             # Identify axis
             ax = _axes[_id_alpha, _id_edd]
             # Loop SMBH mass
             for _id_mass, _mass in enumerate(smbh_masses):
-                print("disk_alpha_viscosity:", _alpha)
-                print("edd:", _edd)
-                print("mass:",_mass)
-                try:
-                    # Generate model
-                    surface_density_func, aspect_ratio_func, opacity_func, pAGN_model, bonus_structures = \
-                        construct_disk_pAGN(
-                            disk_model_name,
-                            _mass,
-                            disk_radius_outer,
-                            _alpha,
-                            _edd,
-                            rad_efficiency,
-                        )
-                    # Identify label
-                    if (_id_alpha == 0) and (_id_edd == 0):
-                        label = "$M_{\mathrm{SMBH}} = 10^{%0.1f}$"%(np.log10(_mass))
-                    else:
-                        label = None
-                    # plot things
-                    ax.plot(np.log10(bonus_structures['R']),np.log10(bonus_structures['tauV']),label=label)
-                except:
-                    line = "Failed building pAGN disk model for "
-                    line = line + "disk_alpha_viscosity = " + str(_alpha) + "; "
-                    line = line + "edd = " + str(_edd) + "; "
-                    line = line + "mass = " + str(_mass)
-                    print(line)
+                # Identify pAGN model
+                tag = "%s_%s_%s"%(str(_alpha),str(_edd),str(_mass))
+                # Identify label
+                if (_id_alpha == 0) and (_id_edd == 0):
+                    label = "$M_{\mathrm{SMBH}} = 10^{%0.1f}$"%(np.log10(_mass))
+                else:
+                    label = None
+                # Check if the pAGN model is really available
+                if not (tag in surface_density_dict):
+                    # If not, keep the legend consistent and move on
                     ax.plot([],[],label=label)
-                
+                    continue
+                # Otherwise, plot things
+                ax.plot(np.log10(bonus_structures_dict[tag]['R']),np.log10(bonus_structures_dict[tag]['tauV']),label=label)
+                if (_id_alpha == 0) and (_id_edd == 0):
+                    print(np.log10(bonus_structures_dict[tag]['tauV']))
+                    tau_drop_mask = (np.log10(bonus_structures_dict[tag]['tauV']) < 2.0) & \
+                        (np.log10(bonus_structures_dict[tag]['R']) > 3)
+                    tau_drop_index = np.argmax(tau_drop_mask)
+                    print(
+                        np.log10(_mass),
+                        np.log10(bonus_structures_dict[tag]['R'][tau_drop_index]),
+                        np.log10(bonus_structures_dict[tag]['tauV'][tau_drop_index]),
+                        )
+
+
+
+            # Vertical line for disk_radius_outer
+            ylims = ax.get_ylim()
+            ax.vlines(np.log10(disk_radius_outer), ylims[0],ylims[1], color='black', label="Truncate", linestyle='dashed')
+            ax.set_ylim(ylims)
+
             # Generate a title
             title = r"$ \alpha = %0.3f; \mathrm{Edd} = %0.3f$"%(_alpha, _edd)
             ax.set_title(title)
@@ -258,20 +271,20 @@ def Vera_batch_tauV(
             # axis labels
             ax.set_xlabel(r"$\log_{10}(R)$")
             ax.set_ylabel(r"$\log_{10}(\tau_V)$")
-            
     # show plots
     #plt.tight_layout()
     savename = "pAGN_%s_disk_tauV.png"%(disk_model_name)
     fig.savefig(savename)
     #plt.show()
+    plt.close()
+
+
 
     
 
 def main():
-    ROS_plots()
-    Vera_batch_rho()
-    Vera_batch_T()
-    Vera_batch_tauV()
+    #ROS_plots()
+    pAGN_model_batch()
 
 if __name__ == "__main__":
     main()
